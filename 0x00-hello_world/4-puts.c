8cd2a57cf6810c6d9079dd5f21f3776e504f09a0
Seif Allah <saifeldarageely@gmail.com>
2023-07-13 06:05:11 -0700

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c programm that prints with puts
+ *
+ * Reaturn: it returns 0 if succeded
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
