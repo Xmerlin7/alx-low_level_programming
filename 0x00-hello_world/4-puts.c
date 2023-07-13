e7f9d9bb8585e0fb3bf250ca20ca01cb355e039c
Seif Allah <saifeldarageely@gmail.com>
2023-07-13 06:15:36 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: a c programm that prints with puts
- *
- * Reaturn: it returns 0 if succeded
-*/
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
