e960572b088f266abfecbb5feb8a1a1c4038a446
Seif Allah <saifeldarageely@gmail.com>
2023-07-13 06:54:29 -0700

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd>
+/**
+ * main - Entery point
+ * Return 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, 59);
+	return (1);
+}
