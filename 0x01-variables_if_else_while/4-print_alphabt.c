7954c55a49ba3fbf63a6a2e7c950fb8aa1a909a1
Seif Allah <saifeldarageely@gmail.com>
2023-07-14 05:20:15 -0700

    loops

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: Prints the alpahpet without q and e
- * return: 0
+ * main - Prints the alphabet without q & e.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -18,6 +19,6 @@ int main(void)
 		putchar(ch);
 		ch++;
 	}
-	putchar('n');
+	putchar('\n');
 	return (0);
 }
