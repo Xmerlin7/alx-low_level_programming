35c3867bba82f51f84d4232108adc35b99d8db7f
Seif Allah <saifeldarageely@gmail.com>
2023-07-19 12:52:23 -0700

    nested

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - using putchar it prints from a to z
+ */
+
+void print_alphabet(void)
+{
+        int i, ctr = 0;
+	
+	while (ctr < 10)
+	{
+        for (i = 'a'; i <= 'z'; i++)
+                _putchar(i);
+        _putchar('\n');
+	ctr++;
+	}
+}
