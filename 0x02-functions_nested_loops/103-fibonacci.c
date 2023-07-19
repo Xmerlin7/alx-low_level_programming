e056cc9c0c9d3c0830e39dc452358a4ea8530337
Seif Allah <saifeldarageely@gmail.com>
2023-07-19 15:41:54 -0700

    nested

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - prints sum of even fibonacci till 4000000
+*Return: Always 0
+*/
+int main(void)
+{
+	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+
+	while (n3 <= 4000000)
+	{
+	n1 = n2;
+	n2 = n3;
+	n3 = n1 + n2;
+	if ((n1 % 2) == 0)
+		sum += n1;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
