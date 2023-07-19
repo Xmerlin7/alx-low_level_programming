5c34c6f1829741083eaaf2f4e92a3f73385b178a
Seif Allah <saifeldarageely@gmail.com>
2023-07-19 15:30:38 -0700

    nested

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - prints first 98 fibonacci numbers
+*Return: return 0
+*/
+int main(void)
+{
+	int i = 0, j = 1 ,k;
+	
+	k = i + j;
+	for (l = 3; l <= 98; i++)
+	{
+		printf("%d, ", k);
+		i = j;
+		j = k;
+		k = i + j;
+	}
+}
+		
+
+
+
