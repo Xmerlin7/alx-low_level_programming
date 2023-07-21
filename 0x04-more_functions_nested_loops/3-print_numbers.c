154e5a6a921714ca446b66e082ea056af19044db
Seif Allah <saifeldarageely@gmail.com>
2023-07-21 06:50:39 -0700

    more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers -  prints all numbers from 0 to 9
+*
+*Return: returns nothing (void)
+*/
+void print_numbers(void)
+{
+	int n = 48;
+	for (n = 48; n < 58; n ++)
+	{
+		_putchar(n );
+	}
+	_putchar(10);
+}
