69151edcd0f2902182bac981c07fe5da769e98a0
Seif Allah <saifeldarageely@gmail.com>
2023-07-24 12:25:43 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point generates a random password
+ *
+ * Return: the  password
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	srand(time(0));
+	while (i <= 2645)
+	{
+		c = rand() % 128;
+		i += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+	return (0);
+}
