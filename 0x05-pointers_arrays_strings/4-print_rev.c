9af74d81bf0009ce0b38311ea9eca93c93b4d1f7
Seif Allah <saifeldarageely@gmail.com>
2023-07-24 09:03:10 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,3 +14,19 @@
 
 		putchar('\n');
 }
+#include "main.h"
+/**
+ *_strlen - returns the length of a string.
+ * @s: string.
+ *Return: returns lenght.
+ */
+int _strlen(char *s)
+{
+        int ctr, i;
+
+        i = 0;
+        for (ctr = 0; s[ctr] != '\0'; ctr++)
+                i++;
+
+        return (i);
+}
