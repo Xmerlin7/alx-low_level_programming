a09e35c008f8fd4aaed284b972779d4886160dc7
Seif Allah <saifeldarageely@gmail.com>
2023-07-24 12:05:48 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*puts_half - puts half of a string
+*@str: string
+*Return:void
+*/
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;	
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
