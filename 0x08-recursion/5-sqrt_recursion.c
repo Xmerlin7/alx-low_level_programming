8a5e39d1b704636489819e5c74bf472abaf76a41
Seif Allah <saifeldarageely@gmail.com>
2023-08-02 18:32:34 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,9 +2,11 @@
 /**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: number to be used
-*
+*@x: helper number
 * Return: the square root of n
 */
+int square(int n, int x)
+
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
