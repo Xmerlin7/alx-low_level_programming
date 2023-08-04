9efe6b679c7b834e9da9b3bfa5a29123dd492b6d
Seif Allah <saifeldarageely@gmail.com>
2023-08-04 14:05:13 -0700

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+/**
+* main - prints all arguments it receives 
+*@argc: argument countet
+*@argv: argument address
+*Return:  0
+*/
+
+int main(int argc, char *argv[])
+{
+		int i;
+
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+
+		return (0);
+}
