ba44b110333afc26ac8f1a941c85a64485bcbc30
Seif Allah <saifeldarageely@gmail.com>
2023-08-12 08:50:09 -0700

    malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,21 +9,22 @@
 * Return: NULL in case of error, pointer to allocated
 * space
 */
-char *_strdup(char *str) {
+char *_strdup(char *str)
+{
 	int size;
 	char *arr;
 	int i;
 	
 	if (str == NULL)
-		return NULL;
+		return(NULL);
 
 	for (size = 0; str[size]; size++);
 	arr = malloc(size + 1);
 	if (arr == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = str[i];
 	arr[size] = '\0';
-	return arr;
+	return (arr);
 }
