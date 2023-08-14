e16bd6382803b3654d6506d113e9eb63b0b2596c
Seif Allah <saifeldarageely@gmail.com>
2023-08-14 09:21:44 -0700

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,8 +4,8 @@
  * init_dog - a function that initializes the dog
  * @d: object of the dog
  * @name: name of the dog
- * @age of the dog
- * @owner of the dog
+ * @age: of the dog
+ * @owner: of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
