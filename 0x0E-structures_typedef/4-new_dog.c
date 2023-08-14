56e70e22ced6ddd559238c7ae0ae7772b076e21e
Seif Allah <saifeldarageely@gmail.com>
2023-08-14 10:53:32 -0700

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -41,29 +41,33 @@ char *_strcopy(char *dest, char *src)
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+dog_t *dog;
 
-	if (name == NULL || age < 0 || owner == NULL)
-	return (NULL);
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	dog->name = malloc(_strlen(name) + 1);
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(_strlen(owner) + 1);
-	if (dog->owner == NULL)
-	{
-		free(dog);
-		free(name);
-		return (NULL);
-	}
-	dog->name = _strcopy(dog->name, name);
-	dog->age = age;
-	dog->owner = _strcopy(doggo->owner, owner);
-	return (dog);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+
+dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+
+dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+
+dog->name = _strcopy(dog->name, name);
+dog->age = age;
+dog->owner = _strcopy(dog->owner, owner);
+
+return (dog);
 }
