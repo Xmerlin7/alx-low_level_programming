d3daa652bef22125db60b4c1d04e3c2beeb2217c
Seif Allah <saifeldarageely@gmail.com>
2023-08-14 09:07:03 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
 *struct dog - a new type of data
 *representing a dog
@@ -7,9 +12,9 @@
 */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 
