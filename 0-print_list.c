b1b971569ab3fe3a8b8b974b4fffa809cd5cb951
Seif Allah <saifeldarageely@gmail.com>
2023-08-24 17:24:03 -0700

    print list

---
+++ 0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a list_t
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h){
+
+	size_t *pointer = h;
+	size_t ctr = 0;
+
+	while (pointer)
+	{
+		if (pointer->str)
+			printf("[%d] %s\n", pointer->len, pointer->str);
+		else
+			printf("[0] (nil)\n");
+
+		ctr += 1;
+		pointer = pointe->next;
+	}
+	return (count);
+}
