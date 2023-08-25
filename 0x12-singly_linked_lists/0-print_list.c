0032368181ef3fe4208120ccd7d071c8f23a967d
Seif Allah <saifeldarageely@gmail.com>
2023-08-24 17:38:15 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,14 +7,14 @@
 *
 *Return: the number of nodes
 */
-size_t print_list(const list_t *h){
-
+size_t print_list(const list_t *h)
+{
 	const list_t *pointer = h;
 	size_t ctr = 0;
 
 	while (pointer != NULL)
 	{
-		if (pointer->str != NULL) 
+		if (pointer->str != NULL)
 			printf("[%d] %s\n", pointer->len, pointer->str);
 		else
 			printf("[0] (nil)\n");
