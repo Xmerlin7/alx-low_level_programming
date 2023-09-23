7fc3f14b5997f29831da4db21cdb200cf71b792f
Seif Allah <saifeldarageely@gmail.com>
2023-09-22 23:30:48 -0700

    fix_white_space

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,24 +3,22 @@
 /**
  *_strncat -  the string pointed to by @src to
  *@dest: String that will be appended
- *@src: String to be concatenated 
+ *@src: String to be concatenated
  *@n: number of bytes being concatenated
  *
  * Return: returns poiner to @dest
  */
-
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+	int i = 0;
+	int len = 0;
 
-        int i = 0;
-        int len = 0;
+	while (dest[i++])
+		len++;
 
-        while (dest[i++])
-                len++;
-
-        for (i = 0; src[i] && i < n; i++)
-                dest[len++] = src[i];
-        dest[len] = '\0';
-        return (dest);
+	for (i = 0; src[i] && i < n; i++)
+		dest[len++] = src[i];
+	dest[len] = '\0';
+	return (dest);
 }
 
