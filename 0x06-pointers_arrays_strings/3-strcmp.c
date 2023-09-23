388cd146ea8b4def982ce4fc78b7df4fb7dc810b
Seif Allah <saifeldarageely@gmail.com>
2023-09-22 23:54:12 -0700

    _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+/**
+* _strcmp - Compares two strings.
+* @str1: The first string.
+* @str2: The second string.
+*
+* Return: 1 if the strings are equal, 0 otherwise.
+*/
+int _strcmp(char *str1, char *str2)
+{
+int i, ret;
+
+for (i = 0; *(str1 + i) || *(str2 + i); i++)
+{
+ret = *(str1 + i) - *(str2 + i);
+if (ret != 0)
+break;
+}
+return (ret);
+}
