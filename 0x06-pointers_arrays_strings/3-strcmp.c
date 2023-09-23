7b2b0b605a2c043ea9e25abea47acd6190b0c931
Seif Allah <saifeldarageely@gmail.com>
2023-09-22 23:59:58 -0700

    _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,30 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i, ret;
+	int i = 0, diff = 0;
 
-for (i = 0; *(str1 + i) || *(str2 + i); i++)
-{
-ret = *(str1 + i) - *(str2 + i);
-if (ret != 0)
-break;
-}
-return (ret);
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
+	}
+	return (diff);
 }
