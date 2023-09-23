fc91d03da820816d47de277fdc9bf24da441f72f
Seif Allah <saifeldarageely@gmail.com>
2023-09-22 23:24:43 -0700

    _strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strncat -  the string pointed to by @src to
+ *@dest: String that will be appended
+ *@src: String to be concatenated 
+ *@n: number of bytes being concatenated
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+
+        int i = 0;
+        int len = 0;
+
+        while (dest[i++])
+                len++;
+
+        for (i = 0; src[i] && i < n; i++)
+                dest[len++] = src[i];
+        dest[len] = '\0';
+        return (dest);
+}
+
